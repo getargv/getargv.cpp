86942cf6c7c9938d11a437cd9f7f7debb161eb3b
Camden Narzt <c.narzt@me.com>
2023-09-27 15:08:53 +0100

    try to make child notice parent dying, and die itself

--- test/src/child.cpp
+++ test/src/child.cpp
@@ -2,11 +2,25 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <dispatch/dispatch.h>
 #include <unistd.h>
+
+void exitOnParentExit(void) {
+  dispatch_source_t source = dispatch_source_create(
+                                                    DISPATCH_SOURCE_TYPE_PROC, getppid(), DISPATCH_PROC_EXIT, NULL);
+
+  dispatch_source_set_event_handler(source, ^{
+    (void)source; // capture so lifetime is long enough
+    exit(0);
+  });
+
+  dispatch_resume(source);
+}
 
 int main() {
   pid_t pid = getppid();
 
+  exitOnParentExit();
   // signal to parent that child is ready
   if (0 != kill(pid, SIGCONT)) {
     perror("child");
